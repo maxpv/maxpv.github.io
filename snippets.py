1a5091af10eb9dae1910792cf02aaeb3a24cb75e
Maxence P <maxence.prevost@gmail.com>
2018-03-21 10:25:29 +0100

    Added uniqueness_overview

--- snippets.py
+++ snippets.py
@@ -9,8 +9,9 @@ def uniqueness_overview(df, columns=None):
     """
     if columns is None:
         columns = df.columns
+        
+    max_column_width = str(len(max(columns, key=len)))
     for column in columns:
         uniques = len(df[column].value_counts())
-        print("col. {0:8}: {1:6} uniques values ({2:2.2f}%) ".format(column, uniques, (uniques / len(df[column])) * 100, ))
-
-
+        s="col. {0:"+max_column_width+"}: {1:6} uniques values ({2:2.2f}%)"
+        print(s.format(column, uniques, (uniques / len(df[column])) * 100, ))
