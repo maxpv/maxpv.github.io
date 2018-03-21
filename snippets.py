131de78a71c25c2a8fe58559ee62ee3a4f3ce119
Maxence P <maxence.prevost@gmail.com>
2018-03-21 10:27:31 +0100

    added CyclicClockTransformer

--- snippets.py
+++ snippets.py
@@ -15,3 +15,17 @@ def uniqueness_overview(df, columns=None):
         uniques = len(df[column].value_counts())
         s="col. {0:"+max_column_width+"}: {1:6} uniques values ({2:2.2f}%)"
         print(s.format(column, uniques, (uniques / len(df[column])) * 100, ))
+        
+### Feature engineering
+
+# This is not optimal for large datasets
+from sklearn.base import BaseEstimator, TransformerMixin
+class CyclicClockTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        print(type(X[0][0]))
+        seconds = np.array([(d.minute * 60 + d[0].hour*60*60) for d in pd.DatetimeIndex(X)])
+        sec_cos = np.sin(2*np.pi*seconds/24*60*60)
+        sec_sin = np.cos(2*np.pi*seconds/24*60*60)
+        return [sec_cos, sec_sin]
